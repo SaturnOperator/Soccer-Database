dde63410f5171f56c733949597d5694688113307
Abdullah Mostafa <17735907+MoonOperator@users.noreply.github.com>
2024-04-11 10:09:25 -0400

    Optimized queries

--- queries.py
+++ queries.py
@@ -29,10 +29,17 @@ The following is the connection information for this project. These settings are
 You must NOT change these settings - by default, db_host, db_port and db_username are as follows when first installing and utilizing psql.
 For the user "postgres", you must MANUALLY set the password to 1234.
 '''
+# root_database_name = "project_database"
+# query_database_name = "query_database"
+# db_username = 'postgres'
+# db_password = '1234'
+# db_host = 'localhost'
+# db_port = '5432'
+
 root_database_name = "project_database"
 query_database_name = "query_database"
-db_username = 'postgres'
-db_password = '1234'
+db_username = ''
+db_password = ''
 db_host = 'localhost'
 db_port = '5432'
 
@@ -63,6 +70,7 @@ def load_database(cursor, conn):
     port = db_port
     conn = psycopg.connect(dbname=dbname, user=user, password=password, host=host, port=port)
     cursor = conn.cursor()
+    return conn  ## @@@@@@@@@@@ DELETE THIS LINE
     
     # Import the dbexport.sql database data into this database
     try:
@@ -79,6 +87,8 @@ def load_database(cursor, conn):
 # Dropping the Database after Query n Execution - Do NOT Modify
 #================================================
 def drop_database(cursor, conn):
+    print("NOT DROPPING TABLE - DELETE THIS EDIT")
+    return ## @@@@@@@@ DELETE THIS LINE
     # Drop database if it exists.
     try:
         conn.autocommit = True
@@ -168,7 +178,13 @@ def Q_1(cursor, conn, execution_time):
     #==========================================================================
     # Enter QUERY within the quotes:
 
-    query = """ """
+    query = """
+SELECT p.player_name, AVG(e16.xg_score) AS avg_xg_score
+FROM player p
+JOIN event_16 e16 ON p.player_id = e16.player_id
+WHERE e16.match_id IN (3764440, 3764661, 3773369, 3773372, 3773377, 3773386, 3773387, 3773403, 3773415, 3773428, 3773457, 3773466, 3773474, 3773477, 3773497, 3773523, 3773526, 3773547, 3773552, 3773565, 3773571, 3773585, 3773586, 3773587, 3773593, 3773597, 3773625, 3773631, 3773656, 3773660, 3773661, 3773665, 3773672, 3773689, 3773695)
+GROUP BY p.player_name
+ORDER BY avg_xg_score DESC;"""
 
     #==========================================================================
 
@@ -187,7 +203,13 @@ def Q_2(cursor, conn, execution_time):
     #==========================================================================    
     # Enter QUERY within the quotes:
 
-    query = """ """
+    query = """
+SELECT p.player_name, COUNT(e16.event_id) AS shots_count
+FROM player p
+JOIN event_16 e16 ON p.player_id = e16.player_id
+WHERE e16.match_id IN (3764440, 3764661, 3773369, 3773372, 3773377, 3773386, 3773387, 3773403, 3773415, 3773428, 3773457, 3773466, 3773474, 3773477, 3773497, 3773523, 3773526, 3773547, 3773552, 3773565, 3773571, 3773585, 3773586, 3773587, 3773593, 3773597, 3773625, 3773631, 3773656, 3773660, 3773661, 3773665, 3773672, 3773689, 3773695)
+GROUP BY p.player_name
+ORDER BY shots_count DESC;"""
 
     #==========================================================================
 
@@ -206,7 +228,14 @@ def Q_3(cursor, conn, execution_time):
     #==========================================================================    
     # Enter QUERY within the quotes:
     
-    query = """ """
+    query = """
+SELECT p.player_name, COUNT(e16.event_id) AS first_time_shots
+FROM player p
+JOIN event_16 e16 ON p.player_id = e16.player_id
+WHERE e16.match_id IN (15946, 15956, 15973, 15978, 15986, 15998, 16010, 16023, 16029, 16056, 16073, 16079, 16086, 16095, 16109, 16120, 16131, 16136, 16149, 16157, 16173, 16182, 16190, 16196, 16205, 16215, 16231, 16240, 16248, 16265, 16275, 16289, 16306, 16317, 303377, 303400, 303421, 303430, 303451, 303470, 303473, 303479, 303487, 303493, 303504, 303516, 303517, 303524, 303532, 303548, 303596, 303600, 303610, 303615, 303634, 303652, 303664, 303666, 303674, 303680, 303682, 303696, 303700, 303707, 303715, 303725, 303731, 3764440, 3764661, 3773372, 3773377, 3773386, 3773387, 3773403, 3773415, 3773428, 3773457, 3773466, 3773474, 3773477, 3773497, 3773523, 3773526, 3773547, 3773552, 3773565, 3773571, 3773585, 3773586, 3773587, 3773593, 3773597, 3773625, 3773631, 3773656, 3773660, 3773661, 3773665, 3773672, 3773689, 3773695)
+AND e16.first_time = TRUE
+GROUP BY p.player_name
+ORDER BY first_time_shots DESC;"""
 
     #==========================================================================
 
@@ -224,7 +253,13 @@ def Q_4(cursor, conn, execution_time):
     #==========================================================================    
     # Enter QUERY within the quotes:
     
-    query = """ """
+    query = """
+SELECT t.team_name, COUNT(e30.event_id) AS passes_count
+FROM team t
+JOIN event_30 e30 ON t.team_id = e30.team_id
+WHERE e30.match_id IN (3764440, 3764661, 3773369, 3773372, 3773377, 3773386, 3773387, 3773403, 3773415, 3773428, 3773457, 3773466, 3773474, 3773477, 3773497, 3773523, 3773526, 3773547, 3773552, 3773565, 3773571, 3773585, 3773586, 3773587, 3773593, 3773597, 3773625, 3773631, 3773656, 3773660, 3773661, 3773665, 3773672, 3773689, 3773695)
+GROUP BY t.team_name
+ORDER BY passes_count DESC;"""
 
     #==========================================================================
 
@@ -242,7 +277,13 @@ def Q_5(cursor, conn, execution_time):
     #==========================================================================    
     # Enter QUERY within the quotes:
     
-    query = """ """
+    query = """
+SELECT p.player_name, COUNT(e30.recipient_id) AS recipient_count
+FROM player p
+JOIN event_30 e30 ON p.player_id = e30.recipient_id
+WHERE e30.match_id IN (3749052, 3749068, 3749079, 3749108, 3749117, 3749133, 3749153, 3749192, 3749196, 3749233, 3749246, 3749253, 3749257, 3749274, 3749276, 3749278, 3749296, 3749310, 3749346, 3749358, 3749360, 3749403, 3749431, 3749434, 3749448, 3749453, 3749454, 3749462, 3749465, 3749493, 3749522, 3749526, 3749528, 3749552, 3749590, 3749603, 3749631, 3749642)
+GROUP BY p.player_name
+ORDER BY recipient_count DESC;"""
 
     #==========================================================================
 
@@ -260,7 +301,13 @@ def Q_6(cursor, conn, execution_time):
     #==========================================================================    
     # Enter QUERY within the quotes:
     
-    query = """ """
+    query = """
+SELECT t.team_name, COUNT(e16.event_id) AS shots_count
+FROM team t
+JOIN event_16 e16 ON t.team_id = e16.team_id
+WHERE e16.match_id IN (3749052, 3749068, 3749079, 3749108, 3749117, 3749133, 3749153, 3749192, 3749196, 3749233, 3749246, 3749253, 3749257, 3749274, 3749276, 3749278, 3749296, 3749310, 3749346, 3749358, 3749360, 3749403, 3749431, 3749434, 3749448, 3749453, 3749454, 3749462, 3749465, 3749493, 3749522, 3749526, 3749528, 3749552, 3749590, 3749603, 3749631, 3749642)
+GROUP BY t.team_name
+ORDER BY shots_count DESC;"""
 
     #==========================================================================
 
@@ -278,7 +325,14 @@ def Q_7(cursor, conn, execution_time):
     #==========================================================================    
     # Enter QUERY within the quotes:
     
-    query = """ """
+    query = """
+SELECT p.player_name, COUNT(e30.event_id) AS through_balls_count
+FROM event_30 e30
+JOIN player p ON e30.player_id = p.player_id 
+WHERE e30.match_id IN (3764440, 3764661, 3773369, 3773372, 3773377, 3773386, 3773387, 3773403, 3773428, 3773457, 3773466, 3773474, 3773477, 3773497, 3773523, 3773526, 3773547, 3773552, 3773565, 3773571, 3773585, 3773586, 3773587, 3773593, 3773597, 3773625, 3773631, 3773656, 3773660, 3773661, 3773665, 3773672, 3773689, 3773695) 
+AND e30.through_ball = True
+GROUP BY p.player_name
+ORDER BY through_balls_count DESC;"""
 
     #==========================================================================
 
@@ -296,7 +350,14 @@ def Q_8(cursor, conn, execution_time):
     #==========================================================================    
     # Enter QUERY within the quotes:
     
-    query = """ """
+    query = """
+SELECT t.team_name, COUNT(e30.event_id) AS through_balls_count
+FROM event_30 e30
+JOIN team t ON e30.team_id = t.team_id
+WHERE e30.match_id IN (3764440, 3764661, 3773369, 3773372, 3773377, 3773386, 3773387, 3773403, 3773428, 3773457, 3773466, 3773474, 3773477, 3773497, 3773523, 3773526, 3773547, 3773552, 3773565, 3773571, 3773585, 3773586, 3773587, 3773593, 3773597, 3773625, 3773631, 3773656, 3773660, 3773661, 3773665, 3773672, 3773689, 3773695)
+AND e30.through_ball = True
+GROUP BY t.team_name
+ORDER BY through_balls_count DESC;"""
 
     #==========================================================================
 
@@ -314,7 +375,14 @@ def Q_9(cursor, conn, execution_time):
     #==========================================================================    
     # Enter QUERY within the quotes:
     
-    query = """ """
+    query = """
+SELECT p.player_name, COUNT(e14.event_id) AS completed_dribbles
+FROM player p
+JOIN event_14 e14 ON p.player_id = e14.player_id
+WHERE e14.match_id IN (15946, 15956, 15973, 15978, 15986, 15998, 16010, 16023, 16029, 16056, 16073, 16079, 16086, 16095, 16109, 16120, 16131, 16136, 16149, 16157, 16173, 16182, 16190, 16196, 16205, 16215, 16231, 16240, 16248, 16265, 16275, 16289, 16306, 16317, 303377, 303400, 303421, 303430, 303451, 303470, 303473, 303479, 303487, 303493, 303504, 303516, 303517, 303524, 303532, 303548, 303596, 303600, 303610, 303615, 303634, 303652, 303664, 303666, 303674, 303680, 303682, 303696, 303700, 303707, 303715, 303725, 303731, 3764440, 3764661, 3773369, 3773372, 3773377, 3773386, 3773387, 3773403, 3773415, 3773428, 3773457, 3773466, 3773474, 3773477, 3773497, 3773523, 3773526, 3773547, 3773552, 3773565, 3773571, 3773585, 3773586, 3773587, 3773593, 3773597, 3773625, 3773631, 3773656, 3773660, 3773661, 3773665, 3773672, 3773689, 3773695)
+AND e14.complete = TRUE
+GROUP BY p.player_name
+ORDER BY completed_dribbles DESC;"""
 
     #==========================================================================
 
@@ -332,7 +400,13 @@ def Q_10(cursor, conn, execution_time):
     #==========================================================================    
     # Enter QUERY within the quotes:
     
-    query = """ """
+    query = """
+SELECT p.player_name, COUNT(e39.event_id) AS dribbled_past
+FROM player p
+JOIN event_39 e39 ON p.player_id = e39.player_id
+WHERE e39.match_id IN (3764440, 3764661, 3773369, 3773372, 3773377, 3773386, 3773387, 3773403, 3773415, 3773428, 3773457, 3773466, 3773474, 3773477, 3773497, 3773523, 3773526, 3773547, 3773552, 3773565, 3773571, 3773585, 3773586, 3773587, 3773593, 3773597, 3773625, 3773631, 3773656, 3773660, 3773661, 3773665, 3773672, 3773689, 3773695)
+GROUP BY p.player_name
+ORDER BY dribbled_past ASC;"""
 
     #==========================================================================
 
@@ -362,6 +436,8 @@ def run_queries(cursor, conn, dbname):
 
     for i in range(10):
         print(execution_time[i])
+
+    print("SUM:", sum([float(ms.split(":")[1].split(" ms")[0]) for ms in execution_time]))
 
 ''' MAIN '''
 try:
